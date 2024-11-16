114f79cf725773f9d1d509ac3b0fbdca5e4347a4
vinay <vinaya.reddy@mercedes-benz.com>
2024-11-16 13:04:13 +0530

    deleted

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'time-capsule',
-  location: 'us-central1'
-};
-
